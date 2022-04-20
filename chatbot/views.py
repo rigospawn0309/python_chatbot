8aa62eb5fefa9c3a012ffae99f131f3e4dbb8b04
Rodrigo <rigospawn@gmail.com>
2022-04-20 22:17:42 +0200

    chatbot 2.0

--- chatbot/views.py
+++ chatbot/views.py
@@ -15,25 +15,44 @@ bot = ChatBot('chatbot',read_only=False,logic_adapters=[
 
 list_to_train = [
     "hi", #Pregunta
-    "hi there", #Respuesta
-    "what is your name",
-    "I'm chatbot",
-    "food",
-    "womens",
-    ""
+    "Hello, do you know each others?",#Respuesta
+    "Hello, my name is John", 
+    "Nice to meet you, John! Do you want to make an appointment for your car review?",
+    "Yes, I am",
+    "When would you like to do it?",
+    "No",
+    "Ok, have you another question?",
+    "16th of August",
+    "I have these times available: 8:30, 10:30, 11:00 and 12:00",
+    "8:30 is ok",
+    "Great your appointment code is TIAGD00!",
+    "8:30",
+    "Great your appointment code is TIAGD00!",
+    "10:30 is ok",
+    "Great your appointment code is TIAGD00!",
+    "10:30",
+    "Great your appointment code is TIAGD00!",
+    "11:00 is ok",
+    "Great your appointment code is TIAGD00!",
+    "11:00",
+    "Great your appointment code is TIAGD00!",
+    "12:00 is ok",
+    "Great your appointment code is TIAGD00!",
+    "12:00",
+    "Great your appointment code is TIAGD00!",
+    "Thanks",
+    "Thanks for trusting us"
 ]
 
 chatterBotCorpusTrainer = ChatterBotCorpusTrainer(bot)
 
-#list_trainer = ListTrainer(bot)
-#list_trainer.train(list_to_train)
+list_trainer = ListTrainer(bot)
+list_trainer.train(list_to_train)
 chatterBotCorpusTrainer.train('chatterbot.corpus.spanish')
 chatterBotCorpusTrainer.train('chatterbot.corpus.english')
 
 def index(request):
     return render(request, 'chatbot/index.html')
-def specific(request):
-    return HttpResponse("This is the specific url")
 def getResponse(request):
     userMessage = request.GET.get('userMessage')
     chatResponse = str(bot.get_response(userMessage))
